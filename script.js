db840e853433e963b6148f846f1321b16ddea686
Hellen Rios <hellenrioss@gmail.com>
2022-01-25 14:24:49 -0300

    adicionando desafio 01

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = "Hellen"
+
+let  num = 2
+
+let currentYear = 2022
+
+let subtraction = currentYear - num
+
+console.log(currentYear + num)
+
+console.log("Estou mexendo no console.log!")
+
+let quartaFeira = false
+
+console.log(typeof quartaFeira);
